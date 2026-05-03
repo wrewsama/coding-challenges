5d85caeba0915763aea5565026b9df201586e2a2
wrewsama <andrewlzs512@gmail.com>
2026-05-03 09:18:22 +0800

    add support for delimiters and nasty disgusting but lowkey funny type hack

--- cut/cut.py
+++ cut/cut.py
@@ -1,8 +1,10 @@
 from typing import IO
 
-def cut(file: IO, field_num: int):
+def cut(file: IO, field_nums: list[int], delimiter: str):
     for line in file:
-        split = line.split()
-        if len(split) < field_num:
+        split = line.split(delimiter)
+        if len(split) < max(field_nums):
             continue
-        print(split[field_num-1]) 
+
+        fields = [split[field_num-1] for field_num in field_nums]
+        print("\t".join(fields))
--- cut/main.py
+++ cut/main.py
@@ -3,14 +3,19 @@ from pathlib import Path
 
 from cut import cut
 
+def _parse_fields(raw_fields: str) -> list[int]:
+    field_delimiter = "," if "," in raw_fields else " "
+    return [int(field) for field in raw_fields.split(field_delimiter)]
+
 def main():
     parser = argparse.ArgumentParser(description="cut from temu")
     parser.add_argument("filepath", type=Path)
-    parser.add_argument("-f", "--field", type=int, required=True, help="which field to print")
+    parser.add_argument("-f", "--fields", type=_parse_fields, required=True)
+    parser.add_argument("-d", "--delimiter", default="\t")
 
     args = parser.parse_args()
     with open(args.filepath) as file:
-        cut(file, args.field)
+        cut(file, args.fields, args.delimiter)
 
 if __name__ == "__main__":
     main()
